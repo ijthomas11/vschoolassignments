a34ea444bbf4694c07121816a703c722f5667e6a
Ishwantis Thomas <ishjthomas@gmail.com>
2021-11-15 22:48:16 -0500

    added timer to go speed clicker but still have to figure out how to stop document click count once timer hits 0

--- go-speed-clicker/index.js
+++ go-speed-clicker/index.js
@@ -1,6 +1,30 @@
- var clickCount = 1
- document.addEventListener("click", function() {
+var header = document.getElementById("header")
+header.style.color = "blue"
+header.style.textAlign = "center"
+var clickerCount = 1
+document.addEventListener("click", function() {
+    clicker.clicks.value = clickerCount++;
+    localStorage.setItem("clickCount", clicker.clicks.value)
 
-     clicker.clicks.value = clickCount++;
-     localStorage.setItem("clickCount", clicker.clicks.value)
- })
+})
+var timer = document.getElementById("timer")
+var time = 10
+
+function getTimer() {
+    timer.textContent = time--
+
+}
+var timerID = setInterval(getTimer, 1000)
+let stopClick = false
+
+function stopTimer() {
+    clearInterval(timerID);
+
+}
+
+
+var timerStop = setTimeout(stopTimer, 11000)
+    // document.addEventListener("click").disabled = true
+    // if (timer === 0) {
+    //     document.off("click")}
+    //figure out how to stop document click after time hits 0
